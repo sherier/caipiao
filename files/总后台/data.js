28f5426b6751d92d64fb1aa4addcef3c090bcf3c
sherier <517563008@qq.com>
2019-05-06 17:06:40 +0800

    commit

---
+++ files/总后台/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk,bl,_(bm,bn,bo,bp)),S,_(),bq,_(),br,g)])),bs,_(),bt,_(bu,_(bv,bw)));}; 
+var b="url",c="总后台.html",d="generationDate",e=new Date(1557129086556.03),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="2a86f97bf8744022a33be75c754f4c1c",q="type",r="Axure:Page",s="name",t="总后台",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="135d6f45533e420a8d2b96a7e8c28420",Y="label",Z="friendlyType",ba="矩形",bb="vectorShape",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=339,bi="height",bj=28,bk="47a56d44a89f4589943889bd4d43c8a6",bl="location",bm="x",bn=16,bo="y",bp=27,bq="imageOverrides",br="generateCompound",bs="masters",bt="objectPaths",bu="135d6f45533e420a8d2b96a7e8c28420",bv="scriptId",bw="u3249";
+return _creator();
+})());
